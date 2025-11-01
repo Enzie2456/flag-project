5b57038c5cb95eb9ff8532ac7ace43857376819e
Adrian Kasapolli <adrian.kasapolli@outlook.com>
2025-11-01 15:25:33 +0100

    Added App.jsx code

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import './App.css'
+import {
+    createBrowserRouter,
+    createRoutesFromElements,
+    Route,
+    RouterProvider,
+} from 'react-router-dom'
+
+// Pages
+import FlagAll, {FlagsLoader} from './pages/FlagAll'
+import FlagDetail, { FlagsDetailLoader} from './pages/FlagDetail'
+import NotFound from './pages/NotFound'
+import FlagError from './pages/profiles/ProfilesError'
+
+// Layouts
+import RootLayout from './layouts/RootLayout'
+import SearchFlagLayout from './layouts/SearchFlagLayout'
+import RegionFilterLayout from './layouts/RegionFilterLayout'
+
+const routesFromElements = createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+        <Route index element={<FlagAll />} errorElement={<FlagError />} loader={FlagsLoader} >
+        <Route
+                path=":id"
+                element={<FlagDetail />}
+                loader={ FlagsDetailLoader}
+            />
+        </Route>
+            <Route path="searchflag" element={<SearchFlagLayout />}>
+            <Route path="regionfilter" element={<RegionFilterLayout />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+
+    </Route>
+)
+
+const router = createBrowserRouter(routesFromElements)
+
+function App() {
+    return (
+        <div className="App">
+            <RouterProvider router={router} />
+        </div>
+    )
+}
+
+export default App
