3ee38ed6f09a9e19aac378d0f878c07d96722f02
Adrian Kasapolli <adrian.kasapolli@outlook.com>
2025-11-17 19:49:22 +0100

    Added Flagdetail - combined the arrow img and back button to a link

--- src/pages/FlagDetail.jsx
+++ src/pages/FlagDetail.jsx
@@ -1,6 +1,6 @@
 import { useLoaderData, Link, useOutletContext } from "react-router-dom";
-import arrowLeftWhite from '../assets/arrow-left.svg'
-import arrowLeftDark from '../assets/arrow-left-dark.svg'
+import arrowLeftWhite from '../assets/arrow-left.svg';
+import arrowLeftDark from '../assets/arrow-left-dark.svg';
 
 export const FlagsDetailsLoader = async ({ params }) => {
   const { id } = params;
@@ -11,7 +11,7 @@ export const FlagsDetailsLoader = async ({ params }) => {
   }
 
   const data = await res.json();
-  return data[0]; // API returnerar en array med ett objekt
+  return data[0];
 };
 
 const FlagDetail = () => {
@@ -20,53 +20,76 @@ const FlagDetail = () => {
 
   return (
     <div className="flag-detail">
-          <div className="arrow-left-content">
-          <img
+
+      {/* ← Back button */}
+      <Link
+        to="/"
+        className="arrow-left-content"
+        style={{
+          textDecoration: "none",
+          color: "inherit",
+          display: "flex",
+          alignItems: "center",
+          gap: "8px"
+        }}
+      >
+        <img
           src={isLight ? arrowLeftDark : arrowLeftWhite}
           alt="arrow left"
           className="arrow-left"
-            />
-      <Link to="/" className="back-button" style={{ textDecoration: 'none' }}>Back</Link>
-          </div>
+        />
+        <span className="back-button">Back</span>
+      </Link>
 
+      {/* CONTENT */}
       <div className="flag-detail-content">
         <img
           src={country.flags.svg}
           alt={country.name.common}
           className="flag-detail-image"
         />
-        
 
         <div className="flag-info">
           <div className="flag-info-column1">
-          <h3>{country.name.common}</h3>
-          <p><strong>Population:</strong> {country.population.toLocaleString()}</p>
-          <p><strong>Region:</strong> {country.region}</p>
-          <p><strong>Capital:</strong> {country.capital?.join(", ")}</p>
-          <p><strong>Native name:</strong> {country.name.nativeName? Object.values(country.name.nativeName)[0].common: country.name.common}</p>
-          </div>
-          <div className="flag-info-column2">
-          <p><strong>Top Level Domain:</strong> {country.tld[0]} </p>
-          <p><strong>Currencies:</strong> {Object.values(country.currencies || {}).map(c => c.name).join(", ")}</p>
-          <p><strong>Languages:</strong> {Object.values(country.languages || {}).join(", ")}</p>
-          </div>
-          <div className="border-countries">
-            <strong>Border Countries:</strong>
-                {country.borders
-                ? country.borders.map((border) => (
-                    <p key={border} className="border-box">
-                      {border}
-                    </p>
-                  ))
-                : " No border countries"}
-            
+            <h3>{country.name.common}</h3>
+            <p><strong>Population:</strong> {country.population.toLocaleString()}</p>
+            <p><strong>Region:</strong> {country.region}</p>
+            <p><strong>Capital:</strong> {country.capital?.join(", ")}</p>
+            <p>
+              <strong>Native name:</strong>{" "}
+              {country.name.nativeName
+                ? Object.values(country.name.nativeName)[0].common
+                : country.name.common}
+            </p>
           </div>
 
+          <div className="flag-info-column2">
+            <p><strong>Top Level Domain:</strong> {country.tld[0]}</p>
+            <p>
+              <strong>Currencies:</strong>{" "}
+              {Object.values(country.currencies || {})
+                .map((c) => c.name)
+                .join(", ")}
+            </p>
+            <p>
+              <strong>Languages:</strong>{" "}
+              {Object.values(country.languages || {}).join(", ")}
+            </p>
+          </div>
+
+          <div className="border-countries">
+            <strong>Border Countries:</strong>
+            {country.borders
+              ? country.borders.map((border) => (
+                  <p key={border} className="border-box">
+                    {border}
+                  </p>
+                ))
+              : " No border countries"}
           </div>
         </div>
       </div>
-          
-
+    </div>
   );
 };
 
